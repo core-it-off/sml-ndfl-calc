3c45d844c26abd1cacf158f8c1e9e317d513276d
Yuriy K <korytov.u@gmail.com>
2020-12-12 18:47:12 +0300

    Add bootstrap and sass

---
+++ src/app/NDFLCalc.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NDFLCalc = (): JSX.Element => {
+    return (
+        <div className="container d-flex flex-column">
+            <div className="alert alert-success" role="alert">
+                <strong>Well done!</strong> Bootstrap and Sass is ready
+            </div>
+        </div>
+    )
+}
+
+export default NDFLCalc;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app/App';
+import './index.scss';
+
+import NDFLCalc from './app/NDFLCalc';
+
+document.title = "NDFL Calculator";
 
 ReactDOM.render(
-  <App />,
+  <NDFLCalc />,
   document.getElementById('root')
 );
